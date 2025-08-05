f4a6e1646f10c8e010183c488c4fb7afafca76cc
HelmiDev <helmipaty@gmail.com>
2025-08-05 15:18:30 +0100

    added

--- components/infinite-logo-scroll.tsx
+++ components/infinite-logo-scroll.tsx
@@ -135,6 +135,8 @@ export default function InfiniteLogoScroll({ durationSeconds = 3, blurPx = 2 }: 
       style={{ filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))" }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      onTouchStart={() => setIsHovered(true)}
+      onTouchEnd={() => setIsHovered(false)}
     >
       <div
         ref={contentRef}
